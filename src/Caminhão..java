d65f61e0dc039ee62200373b185f33b4028b0a29
wedson138-oss <wedson138@gmail.com>
2025-10-23 17:56:41 -0300

    Add Caminhão class extending Veiculo

---
+++ src/Caminhão..java
@@ -0,0 +1,28 @@
+// Integrante: Wedson [Gabriel da Silva]
+public class Caminhão extends Veiculo {
+    private double capacidadeCarga;
+
+    public Caminhão(String placa, String marca, String modelo, int ano, double valor, double capacidadeCarga) {
+        super(placa, marca, modelo, ano, valor);
+        this.capacidadeCarga = capacidadeCarga;
+    }
+
+    public double getCapacidadeCarga() { return capacidadeCarga; }
+    public void setCapacidadeCarga(double capacidadeCarga) { this.capacidadeCarga = capacidadeCarga; }
+
+    @Override
+    public double calcularIpva() {
+        if (capacidadeCarga <= 5) {
+            return getValor() * 0.02;
+        } else {
+            return getValor() * 0.03;
+        }
+    }
+
+    @Override
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        System.out.println("Capacidade de carga: " + capacidadeCarga + " toneladas");
+        System.out.println("-----------------------------------");
+    }
+}
